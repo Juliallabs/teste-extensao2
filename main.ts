8e3f34d9328020d84f38274b64039715852ceece
Julialabs <72475374+Juliallabs@users.noreply.github.com>
2022-11-01 15:12:49 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 namespace ELETROBLOCKS {
+/**
+ * Functions to read eletroblocks sensord.
+ */
+    //% blockId=leituraeletroblocks
     export function leituraAN(sensor: number): number {
         return pins.analogReadPin(AnalogPin.P0)
     }
